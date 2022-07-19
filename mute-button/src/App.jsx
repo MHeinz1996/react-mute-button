3006eb7676037776c1832a01657b767a48c1b7b4
MHeinz1996 <michael.heinzinger@gmail.com>
2022-07-19 14:53:13 -0500

    Completed assignment

---
+++ mute-button/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import ButtonToggle from '../components/ButtonToggle'
+
+function App() {
+  const [toggle, setToggle] = useState(0)
+
+  return (
+    <div className="App">
+      <ButtonToggle toggle={toggle} setToggle={setToggle}/>
+    </div>
+  )
+}
+
+export default App
